a5a128ec1a622deac247dde800a49064e69bab1c
sojung127 <ssojung127@gmail.com>
2020-07-08 21:41:34 +0900

    POJO 클래스  작성

---
+++ src/main/java/ConsolePrinter.java
@@ -0,0 +1,5 @@
+public class ConsolePrinter implements Printer {
+    public void print(String message){
+        System.out.println(message);
+    }
+}
---
+++ src/main/java/Hello.java
@@ -0,0 +1,24 @@
+public class Hello {
+    String name;
+    Printer printer;
+
+    public Hello(){
+
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public void setPrinter(Printer printer){
+        this.printer = printer;
+    }
+
+    public String sayHello(){
+        return "Hello "+name;
+    }
+
+    public void print(){
+        this.printer.print(sayHello());
+    }
+}
