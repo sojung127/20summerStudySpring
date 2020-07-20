2dc43f0d2a7205c36504f6bdc30554004700ae7e
sojung127 <ssojung127@gmail.com>
2020-07-21 02:42:15 +0900

    0720

--- SpringWebProj/src/main/java/myspring/controller/HelloController.java
+++ SpringWebProj/src/main/java/myspring/controller/HelloController.java
@@ -1,5 +1,6 @@
 package myspring.controller;
 
+import myspring.di.xml.Hello;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
